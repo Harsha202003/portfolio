129ba44c6e3d73f468acf27879fa7fb22836cc4f
Udutha HarshaVardhan <chintuudutha20@gmail.com>
2026-03-04 16:29:42 +0530

    changes

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-// Smooth scrolling for anchor links
+// Smooth scrolling for navigation
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
-  });
+
+anchor.addEventListener("click", function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
 });
